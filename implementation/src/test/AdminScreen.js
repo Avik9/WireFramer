67ed91946e250aebbf3b1fdceffc4f8421ed29df
Avik Kadakia <avik.kadakia@stonybrook.edu>
2019-12-05 19:27:13 -0500

    Tried restricting the user to be admin for adminScreen but it did not work

--- implementation/src/test/AdminScreen.js
+++ implementation/src/test/AdminScreen.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { Redirect } from 'react-router-dom';
+// import { firestoreConnect } from 'react-redux-firebase';
 import WireFrameJson from './WireFramerData.json'
 import { getFirestore } from 'redux-firestore';
+// import { firestore } from 'firebase';
 
 class AdminScreen extends React.Component {
 
@@ -36,8 +40,26 @@ class AdminScreen extends React.Component {
     }
 
     render() {
+        // Cannot work because props is not ready before render.
+
+        // console.log("isAdmin: " + this.props.isAdmin);
+        // if(this.props.firebase.profile.isAdmin)
+        // {
+        //     console.log("AdminScreen: Hello Admin!");
+        // }
+        // else
+        // {
+        //     return <Redirect to="/login" />;
+        // }
+        
         return (
             <div>
+                {/* {console.log("AdminScreen: " + this.props.firebase.profile.isAdmin)}
+                {this.props.firebase.profile.isAdmin ? console.log("AdminScreen: Hello Admin!") : <Redirect to="/login" />} }
+
+                {console.log("AdminScreen: " + this.props.isAdmin)}
+                {this.props.isAdmin ? console.log("AdminScreen: Hello Admin!") : <Redirect to="/login" /> */}
+
                 <button onClick={this.handleClear}>Clear Database</button>
                 <button onClick={this.handleReset}>Reset Database</button>
             </div>)
@@ -47,8 +69,9 @@ class AdminScreen extends React.Component {
 const mapStateToProps = function (state) {
     return {
         auth: state.firebase.auth,
-        firebase: state.firebase
+        firebase: state.firebase,
+        isAdmin: state.firebase.profile.isAdmin,
     };
-}
+};
 
-export default connect(mapStateToProps)(AdminScreen);
+export default compose(connect(mapStateToProps))(AdminScreen);
