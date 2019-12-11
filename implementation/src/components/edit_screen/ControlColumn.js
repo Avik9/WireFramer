487251a5d87416a29b6545c45c34eca83487facd
Avik Kadakia <avik.kadakia@stonybrook.edu>
2019-12-11 16:27:24 -0500

    Finished the Control Column

--- implementation/src/components/edit_screen/ControlColumn.js
+++ implementation/src/components/edit_screen/ControlColumn.js
@@ -1,36 +1,48 @@
 import React from 'react';
-
+var sampleContainer = require('./../../images/sampleContainer.png');
+var sampleLabel = require('./../../images/sampleLabel.png');
+var sampleTextfield = require('./../../images/sampleTextfield.png');
+var sampleButton = require('./../../images/sampleButton.png');
 class ControlColumn extends React.Component {
 
     render() {
+        let buttonStyle = {
+            width: "3cm",
+        }
+        let labelStyle = {
+            width: "4cm",
+        }
         return (
             <div className="card z-depth-0">
                 <div className="card-content grey-text text-darken-3 item-card">
                     <div class="row">
-                        <div class="col s12">
-
-                        </div>
+                        <img class="col s12" src={sampleContainer} alt="sampleContainer" />
+                        <br />
                         <span class="card-title center">
                             <strong>Container</strong>
                         </span>
 
-                        <div class="col s12">
-                            <p class="center">Prompt for Input:</p>
-                        </div>
+                        <br /><br />
+                        <div class="col s2" />
+                        <img class="col s1 center" style={labelStyle} src={sampleLabel} alt="sampleContainer" />
+                        <br />
                         <span class="card-title center">
                             <strong>Label</strong>
                         </span>
 
-                        <div class="col s12">
+                        <br /><br />
 
-                        </div>
+                        <div class="col s3" />
+                        <img class="col s1 center" style={buttonStyle} src={sampleButton} alt="sampleContainer" />
+                        <br />
                         <span class="card-title center">
                             <strong>Button</strong>
                         </span>
 
-                        <div class="col s12">
+                        <br /><br />
 
-                        </div>
+                        <img class="col s12" src={sampleTextfield} alt="sampleContainer" />
+                        <br />
                         <span class="card-title center">
                             <strong>Textfield</strong>
                         </span>
